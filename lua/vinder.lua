cf88e1186d4ade92328b8234ee5c35d6fbef2f30
Simeon <aleksov_s@outlook.com>
2021-05-05 13:35:59 +0200

    yaya

--- lua/vinder.lua
+++ lua/vinder.lua
@@ -1,9 +1,6 @@
 local api = vim.api
 local buf, win
-
-
-local api = vim.api
-local buf, win
+local position = 0
 
 local function center(str)
   local width = api.nvim_win_get_width(0)
@@ -11,33 +8,18 @@ local function center(str)
   return string.rep(' ', shift) .. str
 end
 
-local function update_view(direction)
-  api.nvim_buf_set_option(buf, 'modifiable', true)
-
-  position = position + direction
-
-  local result = vim.fn.systemlist('cat ~/.config/nvim/init.lua')
-
-  api.nvim_buf_set_lines(buf, 0, -1, false, result)
-  api.nvim_buf_set_lines(buf, 1, 2, false, {center('HEAD~'..position)})
-  api.nvim_buf_set_lines(buf, 3, -1, false, result)
-
 local function open_window()
-  api.nvim_buf_add_highlight(buf, -1, 'whidSubHeader', 1, 0, -1)
-  vim.api.nvim_buf_set_option(buf, 'modifiable', false)pi.nvim_buf_set_option(buf, 'modifiable', false)
-end
-
   buf = vim.api.nvim_create_buf(false, true)
   local border_buf = vim.api.nvim_create_buf(false, true)
 
   vim.api.nvim_buf_set_option(buf, 'bufhidden', 'wipe')
-  vim.api.nvim_buf_set_option(buf, 'filetype', 'whid')
+  vim.api.nvim_buf_set_option(buf, 'filetype', 'vinder')
 
   local width = vim.api.nvim_get_option("columns")
   local height = vim.api.nvim_get_option("lines")
 
   local win_height = math.ceil(height * 0.8 - 4)
-  local win_width = math.ceil(width * 0.8)
+  local win_width = math.ceil(width * 0.8 - 12)
   local row = math.ceil((height - win_height) / 2 - 1)
   local col = math.ceil((width - win_width) / 2)
 
@@ -73,23 +55,56 @@ end
 
   vim.api.nvim_win_set_option(win, 'cursorline', true)
 
-  api.nvim_buf_set_lines(buf, 0, -1, false, { center('What have i done?'), '', ''})
   api.nvim_buf_add_highlight(buf, -1, 'WhidHeader', 0, 0, -1)
 end
 
-local function vinder() 
-  position = 0
-  open_window()
-  update_view(0)
-  api.nvim_win_set_cursor(win, {4, 0})
+local function update_view(direction)
+  vim.api.nvim_buf_set_option(buf, 'modifiable', true)
+  position = position + direction
+  if position < 0 then position = 0 end
+  local result = vim.fn.systemlist('cat ~/.config/nvim/init.lua')
+  if #result == 0 then table.insert(result, '') end
+
+  api.nvim_buf_set_lines(buf, 1, 2, false, {center('Vinder')})
+  api.nvim_buf_set_lines(buf, 3, -1, false, result)
+
+  api.nvim_buf_add_highlight(buf, -1, 'vinderSubHeader', 1, 0, -1)
+  vim.api.nvim_buf_set_option(buf, 'modifiable', false)
 end
 
 local function close_window()
   api.nvim_win_close(win, true)
 end
+
+local function open_file()
+  local str = api.nvim_get_current_line()
+  close_window()
+  api.nvim_command('edit '..str)
+end
+
+local function move_cursor()
+  local new_pos = math.max(4, api.nvim_win_get_cursor(win)[1] - 1)
+  api.nvim_win_set_cursor(win, {new_pos, 0})
+end
+
+local function set_mappings()
+  local mappings = {
+  }
+end
+
+local function vinder()
+  position = 0
+  open_window()
+  set_mappings()
+  update_view(0)
+  api.nvim_win_set_cursor(win, {4, 0})
+end
+
 return {
   vinder = vinder,
-  open_window = open_window,
   update_view = update_view,
+  open_file = open_file,
+  move_cursor = move_cursor,
   close_window = close_window
 }
+
